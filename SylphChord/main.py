53420316da626d369aab29b9af1f0c48151902f9
Thalivar <tsjpajalol@gmail.com>
2025-07-22 15:21:39 -0700

    Added a volume bar

--- SylphChord/main.py
+++ SylphChord/main.py
@@ -7,6 +7,7 @@ import subprocess
 mpHands = mp.solutions.hands
 mpDrawing = mp.solutions.drawing_utils
 hands = mpHands.Hands(min_detection_confidence = 0.7)
+frameCount = 0
 
 # === Initializes the camera/webcam ===
 wCam = cv2.VideoCapture(0)
@@ -35,8 +36,8 @@ while wCam.isOpened(): # <- Loops while the webcam is open
             x1, y1 = int(tiList[mpHands.HandLandmark.INDEX_FINGER_TIP].x * w), int(tiList[mpHands.HandLandmark.INDEX_FINGER_TIP].y * h)
             x2, y2 = int(tiList[mpHands.HandLandmark.THUMB_TIP].x * w), int(tiList[mpHands.HandLandmark.THUMB_TIP].y * h)
 
-            cv2.circle(frame, (x1, y1), 10, (0, 255, 255), -1)
-            cv2.circle(frame, (x2, y2), 10, (0, 255, 255), -1)
+            cv2.circle(frame, (x1, y1), 10, (255, 0, 255), -1)
+            cv2.circle(frame, (x2, y2), 10, (255, 0, 255), -1)
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
 
             distance = math.hypot(x2 - x1, y2 - y1) # <- Calculates the distance between the thumb and index finger tips
@@ -46,8 +47,15 @@ while wCam.isOpened(): # <- Loops while the webcam is open
             volPercent = int((distance - minDistance) / (maxDistance - minDistance) * 100)
             volPercent = max(0, min(100, volPercent))
 
+            cv2.rectangle(frame, (50, 100), (85, 400), (0, 0, 0), 2) # <- Outline for the volume bar
+            volBarHeight = int((100 - volPercent) * 3)
+            cv2.rectangle(frame, (51, 100 + volBarHeight), (84, 400), (0, 255, 0), -1)
             cv2.putText(frame, f"Volume: {volPercent}%", (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
+            frameCount += 1
+            if frameCount % 10 == 0: # <- Updates the volume every 10 frames, It's to throttle the amixer command
+                setVolume(volPercent)
+            
             setVolume(volPercent)
 
     cv2.imshow("SylphChord", frame) # <- Shows the frame
